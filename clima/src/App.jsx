63a66a180126dba50c0426137c7a068bc5d00680
van-gomes <nninha.gomes@gmail.com>
2025-08-24 17:28:08 -0300

    get API key

--- clima/src/App.jsx
+++ clima/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import Busca from "./components/Busca";
-import ClimaAtual from "./components/ClimaAtual";
-import Previsao from "./components/Previsao";
+import { Busca } from "./components/Busca";
+import { ClimaAtual } from "./components/ClimaAtual";
+import { Previsao } from "./components/Previsao";
 
 function App() {
   const [cidade, setCidade] = useState("");
   const [clima, setClima] = useState(null);
   const [previsao, setPrevisao] = useState([]);
 
+  const apiKey = import.meta.env.VITE_API_KEY || "";
+  console.log("API Key:", apiKey);
+
   return (
     <div>
-      <Titulo>Condições Climáticas</Titulo>
+      <h1>Condições Climáticas</h1>
       <Busca />
       <ClimaAtual />
       <Previsao />
