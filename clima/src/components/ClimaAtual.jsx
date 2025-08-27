377d1f367a1c4d9295dfd47939815d5d588a1614
van-gomes <nninha.gomes@gmail.com>
2025-08-27 16:48:34 -0300

    style ClimaCurrent component

--- clima/src/components/ClimaAtual.jsx
+++ clima/src/components/ClimaAtual.jsx
@@ -1,13 +1,15 @@
+import styles from "./ClimaAtual.module.css";
+
 export function ClimaAtual({ clima }) {
   return (
-    <>
+    <div className={styles.climaInfo}>
       <h3>{clima.name}</h3>
       <img
-        src={`http://openweathermap.org/img/wn/${clima.weather[0].icon}.png`}
+        src={`https://openweathermap.org/img/wn/${clima.weather[0].icon}.png`}
         alt={clima.weather[0].description}
       />
       <p>{clima.main.temp}°C</p>
       <p>{clima.weather[0].description}</p>
-    </>
+    </div>
   );
 }
